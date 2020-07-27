a1018eb0f69baf87b01b50d6140b24b742fcc8ab
zhouxingjie <363908438@qq.com>
2020-07-27 16:06:07 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>HI</h1>
+  <Xing></Xing>
+</template>
+
+<script>
+import Xing from './components/Xing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Xing
+  }
+}
+</script>
---
+++ src/components/Xing.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        我是一个组件，好东西啊
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
