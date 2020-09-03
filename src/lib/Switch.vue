ca08240d19936f32ddc20edfcfedc985feb27ea2
zhouxingjie <363908438@qq.com>
2020-09-03 17:56:39 +0800

    feat:switch 新增切换功能

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,18 @@
 <template>
-  <button><span></span></button>
+  <button :class="{checked}" @click="toogleSwitch"><span></span></button>
 </template>
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+  setup(props) {
+    const checked = ref(false)
+    const toogleSwitch = () => {
+      checked.value = !checked.value
+    }
+    return { checked, toogleSwitch }
+  },
+}
+</script>
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
@@ -8,7 +20,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background-color: blue;
+  background-color: grey;
   border-radius: $h/2;
   position: relative;
   span {
@@ -20,8 +32,11 @@ button {
     background-color: #fff;
     border-radius: $h2/2;
   }
-  &:hover > span {
-    left: calc(100% - #{$h2} - 2px);
+  &.checked {
+    background-color: blue;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
   }
 }
 </style>
